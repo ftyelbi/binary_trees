c4bdccc985215e95a19f67188c3a7c867cf00ae8
ftyelbi <ftyelbi@gmail.com>
2024-01-04 15:53:38 -0800

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,23 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * delete_binary_tree - Deletes an entire binary tree.
- * @tree: A pointer to the root node of the tree to delete.
+ * delete_binary_tree - removes the whole binary tree
+ * @root: pointer to the root node of the tree to delete
  *
  * Description: If the root is NULL, do nothing.
  */
+
 void delete_binary_tree(binary_tree_t *tree)
 {
-	/*Check if the current node is NULL*/
 	if (!tree)
-	return;
-
-	/*Recursively delete the right subtree*/
+		return;
 	delete_binary_tree(tree->right);
-
-	/*Recursively delete the left subtree*/
 	delete_binary_tree(tree->left);
-
-	/* Free the memory of the current node*/
 	free(tree);
 }
