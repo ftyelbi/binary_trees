a5598697a479813a54fd133faf359df282371f39
ftyelbi <ftyelbi@gmail.com>
2024-01-05 12:47:28 -0800

    task 22

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -16,16 +16,16 @@ int binary_tree_is_complete(const binary_tree_t *tree);
  */
 levelorder_queue_t *create_node(binary_tree_t *node)
 {
-	levelorder_queue_t *new_node;
+	levelorder_queue_t *new;
 
-	new_node = malloc(sizeof(levelorder_queue_t));
-	if (!new_node)
+	new = malloc(sizeof(levelorder_queue_t));
+	if (!new)
 		return (NULL);
 
-	new_node->node = node;
-	new_node->next = NULL;
+	new->node = node;
+	new->next = NULL;
 
-	return (new_node);
+	return (new);
 }
 
 /**
