07db8a81705ce7367477f6ba097c1cf4f1bbd7a9
ftyelbi <ftyelbi@gmail.com>
2024-01-05 12:41:21 -0800

    task 21

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		int left = 0, right = 0;
 
 		if (tree->right)
-			right = binary_tree_height(tree->right)+ 1;
+			right = binary_tree_height(tree->right) + 1;
 		if (tree->left)
 			left = binary_tree_height(tree->left) + 1;
 		if (left > right)
