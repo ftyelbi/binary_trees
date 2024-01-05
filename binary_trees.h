0ddad34b9a7af6def70b0a1b605ae13132a509b7
ftyelbi <ftyelbi@gmail.com>
2024-01-04 17:31:36 -0800

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -34,28 +34,28 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 
 /* tasks */
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
+int is_leaf_node(const binary_tree_t *node);
+int is_root_node(const binary_tree_t *node);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *create_binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *insert_left_child(binary_tree_t *parent, int value);
+binary_tree_t *insert_right_child(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-size_t binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
-size_t binary_tree_size(const binary_tree_t *tree);
-size_t binary_tree_leaves(const binary_tree_t *tree);
-size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t measure_binary_tree_height(const binary_tree_t *tree);
+size_t measure_binary_tree_depth(const binary_tree_t *tree);
+size_t measure_binary_tree_size(const binary_tree_t *tree);
+size_t is_leaf_node(const binary_tree_t *tree);
+size_t count_internal_nodes(const binary_tree_t *tree);
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_delete(binary_tree_t *tree);
+void preorder_traversal(const binary_tree_t *tree, void (*func)(int));
+void inorder_traversal(const binary_tree_t *tree, void (*func)(int));
+void postorder_traversalconst binary_tree_t *tree, void (*func)(int));
+void delete_binary_tree(binary_tree_t *tree);
 
 
 /* Advanced tasks */
