084db926fe0a1cafa57db0a8f55493b2691a65e0
ftyelbi <ftyelbi@gmail.com>
2024-01-05 14:03:34 -0800

    task 39

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -10,11 +10,11 @@
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
-	unsigned int i;
-	heap_t (root == NULL || *root = NULL);
+	unsigned int n;
+	heap_t *root = NULL;
 
-	for (i = 0; i < size; i++)
-		heap_insert(&root, array[i]);
+	for (n = 0; n < size; n++)
+		heap_insert(&root, array[n]);
 
 	return (root);
 }
